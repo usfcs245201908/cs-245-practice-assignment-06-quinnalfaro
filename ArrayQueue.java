e0971f4015bf975a5e3ad6e6b432f657783e184a
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-10-10 10:19:39 -0700

    Add files via upload

---
+++ ArrayQueue.java
@@ -0,0 +1,21 @@
+public class ArrayQueue<T> implements Queue {
+	private int head, tail, size = 0;
+	private final int defaultSize = 10;
+
+
+	public ArrayQueue(){
+		T[] a = (T[]) new Object[defaultSize];
+	}
+
+	public T dequeue() throws Exception {
+
+	}
+	public void enqueue(T item) {
+
+
+	}
+	public boolean empty() {
+
+	}
+	
+}
---
+++ ArrayStack.java
@@ -0,0 +1,59 @@
+import java.util.EmptyStackException;
+
+public class ArrayStack implements Stack{
+
+	private final int defaultSize = 10;
+	private int top;
+
+	public ArrayStack() {
+		T[] a = (T[]) new Object[defaultSize];
+		this.top = 0;
+	}
+
+
+
+
+
+	public void push(T item){
+		if(top == a.length - 1){
+			grow_array();
+		}
+		a[++top] = item;
+	}
+
+	public T pop() throws Exception{
+		if(empty()){
+			throw new EmptyStackException();
+		}
+		return(a[top --]);
+	}
+
+	public T peek() throws Exception{
+		if(empty()){
+			throw new EmptyStackException();
+		}
+		return a[top];
+	}
+
+	public boolean empty(){
+		if (top == 0){
+			return true;
+		}
+		return false;
+
+	}
+
+
+
+
+
+	private void grow_array(){
+		T[] new_array = new T[a.length * 2];
+		for(int i = 0; i < a.length; i ++){
+			new_array[i] = a[i];
+		}
+		a = new_array;
+
+	}
+	
+}
